2b34a3841a4ac8bc2e4717ec5de78d04f2d577c8
Joe Tanner <joe.tanner@leadmediapartners.com>
2010-11-10 10:48:30 -0700

    color theme IR_black and some of my tweaks

---
+++ initializers.available/joe.el
@@ -0,0 +1,82 @@
+;; ----------------------------------------
+;;
+;; Visual tweaks
+;;
+;; ----------------------------------------
+
+(color-theme-ir-black)
+
+;; highlight current line
+(global-hl-line-mode 1)
+
+;; font size
+(set-face-attribute 'default nil :height 130) ;; 13pt
+
+;; set default screen size
+(defun set-frame-size-according-to-resolution()
+  (interactive)
+  (if window-system
+      (progn
+        ;; when on a larger display than my 15" screen
+        (if (> (x-display-pixel-width) 1680)
+            (setq default-frame-alist
+                  '((left . 80) (width . 223) (height . 63)))
+          (setq default-frame-alist
+                '((left . 0) (width . 205) (height . 58))))
+        )))
+(set-frame-size-according-to-resolution)
+
+
+
+
+
+;; ----------------------------------------
+;;
+;; Custom mappings
+;;
+;; ----------------------------------------
+
+(global-set-key (kbd "<C-tab>") 'other-window)
+
+;; fast buffer switching
+(global-set-key (kbd "s-{") 'next-buffer)
+(global-set-key (kbd "s-}") 'previous-buffer)
+
+
+
+
+
+
+;; ----------------------------------------
+;;
+;; Custom functions
+;;
+;; ----------------------------------------
+
+;; save all buffers
+(defun force-save-all ()
+  "Unconditionally saves all unsaved buffers."
+  (interactive)
+  (save-some-buffers t))
+;; option-command-s
+(global-set-key (kbd "M-s-ß") 'force-save-all)
+
+;;
+;; Scroller: scrolling without moving the cursor
+;;
+(defun scroll-down-keep-cursor () 
+   ;; Scroll the text one line down while keeping the cursor 
+   (interactive) 
+   (scroll-down 1)) 
+
+(defun scroll-up-keep-cursor () 
+   ;; Scroll the text one line up while keeping the cursor 
+   (interactive) 
+   (scroll-up 1)) 
+
+;; control command p
+(global-set-key (kbd "C-s-<268632080>") 'scroll-down-keep-cursor)
+;; control command n
+(global-set-key (kbd "C-s-<268632078>") 'scroll-up-keep-cursor)
+
+
