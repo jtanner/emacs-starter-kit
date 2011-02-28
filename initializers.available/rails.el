2879354ad953b3fa913fb6a3875cf469402763ef
Joe Tanner <joe.tanner@leadmediapartners.com>
2011-02-28 10:32:19 -0700

    use ruby-mode for .js.rjs

--- initializers.available/rails.el
+++ initializers.available/rails.el
@@ -1,6 +1,8 @@
 ;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/rails-minor-mode"))
 ;; (require 'rails)
 
+(add-to-list 'auto-mode-alist '("\\.js.rjs$" . ruby-mode))
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/rhtml-minor-mode/"))
 (require 'rhtml-mode)
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
