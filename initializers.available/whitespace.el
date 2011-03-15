fcdcf09cfaa846278f3b1ac256af63bf87a4dcb4
Joe Tanner <joe.tanner@leadmediapartners.com>
2011-03-15 09:37:20 -0600

    strip trailing whitespace on ruby files when saving

--- initializers.available/whitespace.el
+++ initializers.available/whitespace.el
@@ -1,3 +1,9 @@
 ;; makes trailing whitespace and tabs obvious
 (global-whitespace-mode t)
 (setq whitespace-style (quote (trailing tabs tab-mark)))
+
+;; adds a hook to strip trailing whitespace automatically on save
+(add-hook 'ruby-mode-hook
+          (lambda()
+            (add-hook 'before-save-hook
+                      'delete-trailing-whitespace nil t)))
